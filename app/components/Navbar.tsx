b38caa7b44d177e2407be4f0566ac4d020ef2c9c
chelahcynthia <cchelangat989@gmail.com>
2023-04-17 21:13:28 +0300

    Created a navbar component

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
+    <div className="prose prose-xl mx-auto
+    flex justify-between flex-col sm:flex-row
+    ">
+
+    </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
@@ -12,4 +12,3 @@ module.exports = {
     require('@tailwindcss/typography'),
   ],
 }
-

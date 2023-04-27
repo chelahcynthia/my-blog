24f0cca7832e573b07aa6abc2c77a4c569e4000c
chelahcynthia <cchelangat989@gmail.com>
2023-04-27 19:58:29 +0300

    Installed React icons to use them on the navbar

--- app/components/MyProfilePic.tsx
+++ app/components/MyProfilePic.tsx
@@ -4,7 +4,7 @@ function MyProfilePic() {
   return (
     <section className="w-full mx-auto">
         <Image className="border-4 border-black dark:border-slate-500 drop-shadow-xl shadow-black rounded-full mx-auto mt-8"
-        src="/images/profile.png"
+        src="/images/profile.jpeg"
         width={200}
         height={200}
         alt="Cynthia Chelah"
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { FaLinkedIn, FaTwitter, FaGithub, FaLaptop} from "react-icons/fa";
 
 function Navbar() {
   return (
@@ -7,7 +8,9 @@ function Navbar() {
         className="prose prose-xl mx-auto
     flex justify-between flex-col sm:flex-row
     ">
+      <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
       <Link href="/" className="text-white/90 no-underline hover:text-white">Cynthia Chelah</Link>
+      </h1>
       </div>
     </nav>
   );

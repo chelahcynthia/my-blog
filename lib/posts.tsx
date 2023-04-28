0dab637b589e29bac4229f44a860c61a976854f5
chelahcynthia <cchelangat989@gmail.com>
2023-04-28 16:57:50 +0300

    Sorted the blogs by date

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -1,39 +1,32 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 
-const postsDirectory =  path.join(process.cwd(), 'blogposts')
+const postsDirectory = path.join(process.cwd(), "blogposts");
 
 export function getPostsData() {
-    const fileNames = fs.readdirSync(postsDirectory);
-    const allPostsData = fileNames.map((fileName) => {
-        // remove ".md" from filename to get id
-        const id = fileName.replace(/\.md$/, '');
-        
-        // Read markdown file as string
-        const fullPath = path.join(postsDirectory, fileName);
-        const fileContents = fs.readFileSync(fullPath, 'utf8')
-        
-        // use gray-matter to parse the post metadata section
-        const matterResult =  matter(fileContents);
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
+    // remove ".md" from filename to get id
+    const id = fileName.replace(/\.md$/, "");
 
-        const blogPost: BlogPost = {
-            id,
-            title: matterResult.data.title,
-            date: matterResult.data.date,
-        }
+    // Read markdown file as string
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
 
-        // combine the data with the id
-        
+    // use gray-matter to parse the post metadata section
+    const matterResult = matter(fileContents);
 
-    
+    const blogPost: BlogPost = {
+      id,
+      title: matterResult.data.title,
+      date: matterResult.data.date,
+    };
 
+    // combine the data with the id
+    return blogPost;
+  });
 
-
-
-
-
-
-    })
-} 
-
+  // sort posts by date
+  return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
+}

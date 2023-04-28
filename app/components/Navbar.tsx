c3dfe75b3259592436cb3d35dc1bfff6ca677616
chelahcynthia <cchelangat989@gmail.com>
2023-04-28 11:27:39 +0300

    Created a types.ts file on the root directory

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import {  FaTwitter, FaGithub} from "react-icons/fa";
+import { FaTwitter, FaGithub } from "react-icons/fa";
 
 function Navbar() {
   return (
@@ -18,13 +18,6 @@ function Navbar() {
           </Link>
         </h1>
         <div className="flex flex-row justify-center sm:justify-evenly align-middle gap-4 text-white text-4xl lg:text-5xl">
-          {/* <Link
-            className="text-white/90 hover:text-white"
-            href="https://www.linkedin.com/in/cynthiachelangat/"
-          >
-            < FaLinkedIn/>
-          </Link> */}
-
           <Link
             className="text-white/90 hover:text-white"
             href="https://github.com/chelahcynthia"

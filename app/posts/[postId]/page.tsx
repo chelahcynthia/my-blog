18b60f33b509c72fc3416725fd31dd6aaa6c814a
chelahcynthia <cchelangat989@gmail.com>
2023-05-01 10:14:47 +0000

    Generated metadata

--- app/posts/[postId]/page.tsx
+++ app/posts/[postId]/page.tsx
@@ -1,17 +1,35 @@
-import { getPostsData } from '@/lib/posts'
-import React from 'react'
-import { notFound } from 'next/navigation'
+import { getPostsData } from "@/lib/posts";
+import React from "react";
+import { notFound } from "next/navigation";
 
-export default async function Post({ params }: { params: {postId: string}}) {
+export function generateMetadata({
+  params,
+}: {
+  params: { postId: string };
+}) {
+  const posts = getPostsData();
+  const { postId } = params;
 
-    const posts = getPostsData()
-    const { postId} = params
+  const post = posts.find((post) => post.id === postId);
 
-    if (!posts.find(post => post.id === postId)) {
-        return notFound()
-    }
+  if (!post) {
+    return {
+      title: "Post Not Found",
+    };
+  }
 
-  return (
-    <div>page</div>
-  )
+  return {
+    title: post.title,
+  };
+}
+
+export default async function Post({ params }: { params: { postId: string } }) {
+  const posts = getPostsData();
+  const { postId } = params;
+
+  if (!posts.find((post) => post.id === postId)) {
+    return notFound;
+  }
+
+  return <div>page</div>;
 }
